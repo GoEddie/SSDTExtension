2f3fcf332b55acd7ffe54a252153d5ff6bffd423
goeddie <goeddieuk@gmail.com>
2014-08-18 20:35:33 +0100

    fixing script properties to return default value

--- src/TSQLHelper/ScriptProperties.cs
+++ src/TSQLHelper/ScriptProperties.cs
@@ -9,12 +9,60 @@ using System.IO;
 
 namespace TSQLHelper
 {
+    public struct ScriptDetail
+    {
+        public string Name;
+        public TSqlTokenType Type;
+    }
+
     public class ScriptProperties
     {
 
+        public ScriptDetail GetScriptDetail(string filename)
+        {
+            var txtRdr = new StreamReader(filename);
+            var parser = new TSql110Parser(true);
+
+            IList<ParseError> errors;
+            var sqlFragment = parser.Parse(txtRdr, out errors);
+
+            var inName = false;
+            var name = "";
+
+            var result = new ScriptDetail();
+
+            foreach (var a in sqlFragment.ScriptTokenStream)
+            {
+
+                switch (a.TokenType)
+                {
+                    case TSqlTokenType.Procedure:
+                        result.Type = a.TokenType;
+                        break;
+                    case TSqlTokenType.QuotedIdentifier:
+                    case TSqlTokenType.Identifier:
+                    case TSqlTokenType.Dot:
+                        inName = true;
+                        name += a.Text;
+
+
+                        break;
+
+                    case TSqlTokenType.WhiteSpace:
+                        if (inName)
+                        {
+                            result.Name = name;
+                            return result;
+                        }
+                        break;
+                }
+            }
+
+            return new ScriptDetail();
+        }
+
         public string GetProcNameFromSqlFile(string filename)
         {
-
             var txtRdr = new StreamReader(filename);
             var parser = new TSql110Parser(true);
 
