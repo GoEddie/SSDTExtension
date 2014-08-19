a9d6b1f5de1d30b4d47df36513ebcfc76b5d3a45
goeddie <goeddieuk@gmail.com>
2014-08-19 18:40:18 +0100

    adding version number to vsixmanifest so it upgrades previous versions

--- src/SqlServerTddHelper/Properties/AssemblyInfo.cs
+++ src/SqlServerTddHelper/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
 
 
 [assembly: InternalsVisibleTo("SqlServerTddHelper_UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010017506e5d918e4b0e799ead8f7da56c7648f66e63f1c22c0b2228ab9c3f52ae70eb965c7683fbe3aab044b78b3e1babd8d2fc13a780259e24ab34538948896e905a5e61091b5858c3d33bde7c9ab0052d4539aa50da4cfedb849e265958604adf16e17eb0310fa9382d7db4e44a90f160a434b34c538c148bdf12b52013caa6a6")]
